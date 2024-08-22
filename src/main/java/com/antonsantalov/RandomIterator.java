b04c559b00fc1baa142df91858857217507c7e10
Anton Santalov <anton.santalov1995@gmail.com>
2024-08-22 22:30:13 +0400

    Code added.

--- src/main/java/com/antonsantalov/RandomIterator.java
+++ src/main/java/com/antonsantalov/RandomIterator.java
@@ -1,2 +1,22 @@
-package com.antonsantalov;public class RandomIterator {
+package com.antonsantalov;
+
+import java.util.Iterator;
+
+class RandomIterator implements Iterator<Integer> {
+
+    private final Randoms integers;
+
+    public RandomIterator(Randoms integers) {
+        this.integers = integers;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return true;
+    }
+
+    @Override
+    public Integer next() {
+        return integers.random.nextInt(integers.max - integers.min + 1) + integers.min;
+    }
 }
--- src/main/java/com/antonsantalov/Randoms.java
+++ src/main/java/com/antonsantalov/Randoms.java
@@ -1,2 +1,21 @@
-package com.antonsantalov;public class Randoms {
+package com.antonsantalov;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected final Random random;
+    protected final int min;
+    protected final int max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+        this.random = new Random();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new RandomIterator(this);
+    }
 }
